6eb63afc9d135fe69700c764ef63de77a109c942
seeun0210 <cocobell3@naver.com>
2025-07-20 15:56:08 +0900

    feat: 지연평가

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,43 @@
 class FxIterable<A> {
-  private iterable: Iterable<A>;
-
-  constructor(iterable: Iterable<A>) {
-    this.iterable = iterable;
-  }
+  constructor(private iterable: Iterable<A>) {}
 
   [Symbol.iterator]() {
     return this.iterable[Symbol.iterator]();
   }
 
-  map<B>(fn: (a: A) => B): FxIterable<B> {
-    const mapped = Array.from(this.iterable).map(fn);
-    return new FxIterable(mapped);
+  map<B>(f: (a: A) => B): FxIterable<B> {
+    const iterable = this.iterable;
+    return new FxIterable({
+      *[Symbol.iterator]() {
+        for (const item of iterable) {
+          yield f(item);
+        }
+      },
+    });
+  }
+
+  filter(f: (a: A) => boolean): FxIterable<A> {
+    const iterable = this.iterable;
+    return new FxIterable({
+      *[Symbol.iterator]() {
+        for (const item of iterable) {
+          if (f(item)) {
+            yield item;
+          }
+        }
+      },
+    });
   }
 }
 
+//헬퍼 함수
 function fx<A>(iterable: Iterable<A>): FxIterable<A> {
   return new FxIterable(iterable);
 }
 
-const mapped = new FxIterable([1, 2, 3, 4, 5]).map((x) => x * 2);
-
-console.log(mapped);
+const mapped = fx([1, 2, 3, 4, 5]).map((x) => x * 2);
 
 const mapped2 = fx(["a", "b", "c", "d", "e"])
   .map((x) => x.toUpperCase())
   .map((b) => b);
-console.log(mapped2);
+console.log([...mapped2]);
